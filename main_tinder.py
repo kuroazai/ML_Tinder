0b59d21e63823be688633d2da0aac291c2531d8b
kuroazai <pyromwansa@gmail.com>
2023-05-08 17:40:02 +0100

    Cleaned up logic & data capturing

--- main_tinder.py
+++ main_tinder.py
@@ -65,6 +65,7 @@ def auto_tinder(number):
                 print(ValueError)
 
             if args.validation == 1:
+                print('Validation mode')
                 continue
 
             if val == 0:
@@ -115,38 +116,90 @@ def count_files(APP_FOLDER):
     return total_files
 
 
-def data_collection():
+def data_collection(seconds):
+    print(f"please wait for {seconds} seconds")
+    time.sleep(seconds)
+    print("Starting data collection")
     while True:
-        soup = BeautifulSoup(src.driver.page_source, "html.parser")
+        soup = BeautifulSoup(src.browser.page_source, "html.parser")
         divs = soup.find_all("div", class_="Bdrs(8px) Bgz(cv) Bgp(c) StretchedBox")
         # print(len(divs))
         url = ''
+        #print(len(divs), len(divs) / 2)
         if len(divs) >= 2 and url == '':
-            selector = divs[1]
+            n = int(len(divs) / 2)
+            selector = divs[n]
             soup = BeautifulSoup(str(selector), "html.parser")
             div_style = soup.find('div')['style']
             style = cssutils.parseStyle(div_style)
             url = style['background-image']
             url = url.replace('url(', '').replace(')', '')
-            # print('\n', url)
-        # like or dislike
-        try:  # used try so that if user pressed other than the given key error will not be shown
-            if keyboard.is_pressed('right'):
-                # save image into likes
-                file_count = count_files(ML_LIKES)
-                name = ML_LIKES + '/liked_{}.jpg'.format(str(file_count + 1))
-                response = requests.get(url, stream=True)
-                with open(name, 'wb') as out_file:
-                    shutil.copyfileobj(response.raw, out_file)
-            elif keyboard.is_pressed('left'):
-                # save to dislikes
-                file_count = count_files(ML_DISLIKES)
-                name = ML_DISLIKES + '/disliked_{}.jpg'.format(str(file_count + 1))
-                response = requests.get(url, stream=True)
-                with open(name, 'wb') as out_file:
-                    shutil.copyfileobj(response.raw, out_file)
-        except ValueError:
-            print(ValueError)
+            #print('\n\n', url)
+
+        if url == '' or url is None:
+            continue
+
+        if keyboard.is_pressed('right'):
+            print('Liked')
+            # save image into likes
+            file_count = count_files(ML_LIKES)
+            name = ML_LIKES + '/liked_{}.jpg'.format(str(file_count + 1))
+            print(name)
+            print(url)
+            response = requests.get(url, stream=True)
+            with open(name, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+        if keyboard.is_pressed('left'):
+            print('Disliked')
+            file_count = count_files(ML_DISLIKES)
+            name = ML_DISLIKES + '/disliked_{}.jpg'.format(str(file_count + 1))
+            print(name)
+            print(url)
+            response = requests.get(url, stream=True)
+            with open(name, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+
+
+def keyboard_test():
+    print('Press a key')
+    while True:
+        # Wait for a key press event
+        event = keyboard.read_event()
+        # Check if the event is a key press event
+        if event.event_type == 'down':
+            # Get the key value from the event
+            key_value = event.name
+
+            # Check for left/right arrow key presses
+            if key_value == 'left':
+                print('Left arrow key pressed')
+            elif key_value == 'right':
+                print('Right arrow key pressed')
+
+
+def check_folders():
+    if not os.path.exists(LIKES):
+        os.makedirs(LIKES)
+    if not os.path.exists(DISLIKES):
+        os.makedirs(DISLIKES)
+    if not os.path.exists(ML_LIKES):
+        os.makedirs(ML_LIKES)
+    if not os.path.exists(ML_DISLIKES):
+        os.makedirs(ML_DISLIKES)
+
+
+def main(test=False):
+    if test:
+        keyboard_test()
+        return
+    check_folders()
+    src.load_page()
+    if args.mode == 'auto':
+        print("running auto tinder")
+        auto_tinder(cfg.total_likes)
+    elif args.mode == 'data':
+        print("running data collection")
+        data_collection(10)
 
 
 if __name__ == "__main__":
@@ -162,7 +215,4 @@ if __name__ == "__main__":
     parser.add_argument('--mode', type=str, default='data', help='The mode we want to run either data for data collection or auto for automatic swiping')
     parser.add_argument('--validation', type=int, default=1, help='Weahter we want to validate the model or not 1 or 0 for yes or no')
     args = parser.parse_args()
-    if args.mode == 'auto':
-        auto_tinder(cfg.total_likes)
-    elif args.mode == 'data':
-        data_collection()
+    main()
