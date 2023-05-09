b139ebbd85e60bc576e0f61eb08259340f28a4e1
kuroazai <pyromwansa@gmail.com>
2023-05-09 23:54:22 +0100

    Callbacks and tensorboard features added,

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-liked_images = 'path to store liked images'
-disliked_images = 'path to store disliked images'
-mliked_images = 'path to store models liked images'
-mdisliked_images = 'path to store models disliked images'
-firefox_profile = 'your firefox profile'
+import os
+
+current_file_path = os.path.abspath(__file__)
+dir = os.path.dirname(current_file_path)
+
+staging_area = os.path.join(dir, 'training_data/unprocessed')
+liked_images = os.path.join(dir, 'training_data/unprocessed/liked')
+disliked_images = os.path.join(dir, 'training_data/unprocessed/disliked')
+
+mliked_images = os.path.join(dir, 'training_data/validation/liked')
+mdisliked_images = os.path.join(dir, 'training_data/validation/disliked')
+
+firefox_profile = r''
+
 total_likes = 100
 total_runs = 250
